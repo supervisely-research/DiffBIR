a3cb687b7f5b95fd1025c675ac43a23f534bf215
max-unfinity <poldersen2@gmail.com>
2023-09-13 15:36:45 +0000

    test inference

---
+++ download_weights.sh
@@ -0,0 +1,4 @@
+mkdir -p weights
+cd weights
+wget https://huggingface.co/lxq007/DiffBIR/resolve/main/general_full_v1.ckpt
+wget https://huggingface.co/lxq007/DiffBIR/resolve/main/general_swinir_v1.ckpt
